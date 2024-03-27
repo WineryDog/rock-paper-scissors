4dc092a12fcc5b953c3429eacfa5ed66f6001fb5
WineryDog <frank8rsl@gmail.com>
2024-03-27 15:16:23 +0100

    Make UI and rules box

--- jscode.js
+++ jscode.js
@@ -1,6 +1,3 @@
-// Welcome
-console.log("Welcome to Rock Paper Scissors");
-
 // Computer Random Choice
 function getComputerChoice(choices) {
     return choices[Math.floor(Math.random() * choices.length)];
@@ -8,81 +5,75 @@ function getComputerChoice(choices) {
 
 const choices = ["Rock","Paper","Scissors"];
 
-// Player Choice
-function getPlayerChoice() {
-    let newstring = "";
-    while (true) {
-        let playerChoice = prompt('Please enter your selection between "Rock","Paper" and "Scissors"');
-        let allsmall = playerChoice.toLowerCase()
-        let firstletter = allsmall.charAt(0).toUpperCase();
-        let slice1on = allsmall.slice(1);
-        newstring = firstletter + slice1on;
-        if (newstring === "Rock" || newstring === "Paper" || newstring === "Scissors") {
-            return newstring;
-        } else {
-            alert("Invalid User Input. Please try again");
-        }    
-    }
-  }
-
 // Scores
 let playerScore = 0
 let computerScore = 0
 let draws = 0
 
+// Rounds
+let rounds = 0;
+
+let banner = document.querySelector(".banner")
+banner.textContent = "Rounds left: " + 5;
+
+let roundResult = document.querySelector(".round-result")
+roundResult.textContent = "Pick one to start the game"
+
+let currentScore = document.querySelector(".game-status")
+currentScore.textContent = "Player: " + playerScore + " Computer: " + computerScore + " Draws: " + draws;
 
 // Round Function
 function playRound(computerChoice,playerChoice) {
     // DRAW CASE
     if (playerChoice === computerChoice) {
-        console.log(playerChoice + " vs " + computerChoice + " - DRAW!");
+        roundResult.textContent = playerChoice + " vs " + computerChoice + " - DRAW!";
         draws++;
     }
     // WIN CASE
     else if (playerChoice === "Rock" && computerChoice === "Scissors" || playerChoice === "Scissors" && computerChoice === "Paper" || playerChoice === "Paper" && computerChoice === "Rock") {
-        console.log("YOU WIN! " + playerChoice + " beats " + computerChoice);
+        roundResult.textContent = "YOU WIN! " + playerChoice + " beats " + computerChoice;
         playerScore++;
     }
     // LOSE CASE
     else if (computerChoice === "Rock" && playerChoice === "Scissors" || computerChoice === "Scissors" && playerChoice === "Paper" || computerChoice === "Paper" && playerChoice === "Rock") {
-        console.log("YOU LOSE! " + computerChoice + " beats " + playerChoice);
+        roundResult.textContent = "YOU LOSE! " + computerChoice + " beats " + playerChoice;
         computerScore++;
     }
+    currentScore.textContent = "Player: " + playerScore + " Computer: " + computerScore + " Draws: " + draws;
   }
 
-// Rounds
-let rounds = 0
 
-// Game function
-function playgame() {
-    while(true) {
-        if (rounds === 5) {
-            console.log("The final score is: Player = " + playerScore + " Computer = " + computerScore + " Draws = " + draws);
-            if (playerScore > computerScore) {
-                console.log("PLAYER WINS THE MATCH! Congratulations");
-            }
-            if (playerScore < computerScore) {
-                console.log("Computer wins the match, what a shame");
-            }
-            if (playerScore === computerScore) {
-                console.log("No one wins!");
-            }            
-            console.log("Game over, thank you for playing.")
-            console.log("Refresh the page to play again.")
-            break
+function checkEndOfGame() {
+    if (rounds % 5 === 0) {
+        currentScore.textContent = "Player: " + playerScore + " Computer: " + computerScore + " Draws: " + draws;
+        if (playerScore > computerScore) {
+            banner.textContent = "PLAYER WINS THE MATCH! Congratulations";
+        } else if (playerScore < computerScore) {
+            banner.textContent = "Computer wins the match, what a shame";
+        } else {
+            banner.textContent = "No one wins!";
         }
-        else {
-            let computerChoice = getComputerChoice(choices);
-            let playerChoice = getPlayerChoice();
-            playRound(computerChoice,playerChoice);  
-            rounds++   
-            if (rounds !== 5) {
-                console.log("Current score: Player = " + playerScore + " Computer = " + computerScore + " Draws = " + draws);
-            }
-        }
-
+        playerScore = 0;
+        computerScore = 0;
+        draws = 0;
+        rounds = 0;
     }
 }
 
-// Main function
-playgame()
+
+const playerButtons = document.querySelectorAll(".player-btn")
+
+playerButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        let computerChoice = getComputerChoice(choices);
+        let playerChoice = btn.id;
+        playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
+        console.log(playerChoice)
+        playRound(computerChoice, playerChoice);
+        rounds++;
+        banner.textContent = "Rounds left: " + (5 - rounds);
+        if (rounds % 5 === 0) {
+            checkEndOfGame();
+        }
+    });
+});
